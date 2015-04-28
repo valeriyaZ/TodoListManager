02280a4202763085b5a22491ded314ae4e7d3674
valeriyaZ <valeriya.zelikovich@mail.huji.ac.il>
2015-04-28 17:25:46 +0300

    ex4

--- app/src/main/java/todolist/huji/ac/il/todolistmanager/TodoListManagerActivity.java
+++ app/src/main/java/todolist/huji/ac/il/todolistmanager/TodoListManagerActivity.java
@@ -6,12 +6,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.ContentValues;
 
 import android.content.Context;
 import android.net.Uri;
 
 import android.widget.ListView;
-
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import android.app.AlertDialog;
 import java.util.ArrayList;
@@ -19,35 +21,55 @@ import android.widget.AdapterView;
 import android.view.View;
 import android.content.DialogInterface;
 import java.util.Date;
+import android.util.Log;
+import android.database.Cursor;
 
 
 
 public class TodoListManagerActivity extends ActionBarActivity {
 
-    public ArrayList<NewTodoItem> listItems = new ArrayList<>();
-    public myAdapter<NewTodoItem> listAdapter;
+    //public ArrayList<NewTodoItem> listItems = new ArrayList<>();
+
+    public SQLiteDatabase db;
+    public  DBHelper dbHelper;
+    public myCursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list_manager);
-        final ListView todoItems = (ListView)findViewById(R.id.lstTodoItems);
-        listAdapter = new myAdapter<>(this,android.R.layout.simple_list_item_1, listItems);
 
-        todoItems.setAdapter(listAdapter);
+        final ListView todoItems = (ListView)findViewById(R.id.lstTodoItems);
+        dbHelper = new DBHelper(TodoListManagerActivity.this);
+        db = dbHelper.getWritableDatabase();
+
+        Cursor cursor = db.query(DBHelper.TABLE_NAME,null,null,null,null,null,null);
+        cursorAdapter = new myCursorAdapter(this, cursor, 0);
+        todoItems.setAdapter(cursorAdapter);
+
+
 
         todoItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view,
                                            final int position, long id) {
-                final String itemTitle = listItems.get(position).getTitle();
+
+                Cursor cursor = db.query(DBHelper.TABLE_NAME,null,null,null,null,null,null);
+                cursor.moveToPosition(position);
+                final int myId = cursor.getInt(0);
+                final String itemTitle = cursor.getString(1);
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(TodoListManagerActivity.this);
                 builder.setTitle(itemTitle);
                 builder.setNegativeButton(" Delete Item ", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        listItems.remove(position);
-                        listAdapter.notifyDataSetChanged();
+                        DBHelper helper = new DBHelper(TodoListManagerActivity.this);
+                        SQLiteDatabase db = helper.getWritableDatabase();
+                        db.delete(DBHelper.TABLE_NAME, "_id = " + myId, null);
+                        Cursor cursor = db.query(DBHelper.TABLE_NAME,null,null,null,null,null,null);
+                        cursorAdapter.changeCursor(cursor);
+                        cursorAdapter.notifyDataSetChanged();
                     }
                 });
                 String[] curItems = new String[1];
@@ -89,9 +111,17 @@ public class TodoListManagerActivity extends ActionBarActivity {
         if(reqCode == 0 && resCode == RESULT_OK){
             Date curData = (Date)data.getSerializableExtra("dueDate");
             String curString = data.getStringExtra("title");
-            NewTodoItem curItem = new NewTodoItem(curString,curData);
-            listItems.add(curItem);
-            listAdapter.notifyDataSetChanged();
+
+
+            DBHelper helper = new DBHelper(this);
+            SQLiteDatabase db = helper.getWritableDatabase();
+            ContentValues cur = new ContentValues();
+            cur.put("title",curString);
+            cur.put("due", curData.getTime());
+            db.insert(DBHelper.TABLE_NAME,null,cur);
+            Cursor cursor = db.query(DBHelper.TABLE_NAME,null,null,null,null,null,null);
+            cursorAdapter.changeCursor(cursor);
+            cursorAdapter.notifyDataSetChanged();
         }
     }
 
